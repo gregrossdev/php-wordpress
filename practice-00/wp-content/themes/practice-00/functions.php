08a24708c735ece80aca6fcddb82ff36cb1e216f
Gregory Ross <gregross.dev@gmail.com>
2022-11-03 13:16:41 -0400

    destructured admin css

--- practice-00/wp-content/themes/practice-00/functions.php
+++ practice-00/wp-content/themes/practice-00/functions.php
@@ -1,7 +1,7 @@
 <?php
-/* Admin CSS styles */
+/* Admin CSS assets */
 function style_admin() {
-    wp_enqueue_style( 'style-admin', get_stylesheet_directory_uri() . '/style-admin.css' );
+    wp_enqueue_style( 'style-admin', get_stylesheet_directory_uri() . '/assets/css/admin.css');
 }
 
 add_action( 'admin_enqueue_scripts', 'style_admin');
